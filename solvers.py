0e3d559266bbb167c697a4e82b124ae6632b5f55
Ruben Neelissen <rubnee@uni-bremen.de>
2019-06-03 21:44:19 +0200

    Added Matrix-Solver without solution vector

--- solvers.py
+++ solvers.py
@@ -15,4 +15,29 @@ def gaussian_eliminate(aa, bb):
     """
     nn = aa.shape[0]
     xx = np.zeros((nn,), dtype=float)
+
+   
+    
+    """Solving Matrix"""
+    
+    for j in range(0,nn):
+        for i in range(j+1,nn):
+            if aa[i,j] == 0:
+                continue
+            bb[i] = aa[j, j] / aa[i,j] * bb[i] - bb[j]            
+            aa[i] = aa[j, j] / aa[i,j] * aa[i] - aa[j]
+        
+   #for k in range(0,nn,-1):
+   xx[k] = bb[nn]
+    
+    return aa, bb, xx
+    
+    
+    
+aa = np.array([[1.0, 2.0, -1.0], [1.0, 1.0, 2.0], [2.0, 3.0, -3.0]])
+bb = np.array([2.0, 9.0, -1.0])
+
+aa, bb, xx = gaussian_eliminate(aa, bb)
+
+print(aa, bb, xx)
     
