94599974e97fd47f7a2a05e9cce27c0335f59e16
Ruben Neelissen <rubnee@uni-bremen.de>
2019-06-04 11:08:08 +0200

    Added testfunction
    Prevous bug not fixed

--- solvers.py
+++ solvers.py
@@ -13,37 +13,22 @@ def gaussian_eliminate(aa, bb):
         Vector xx with the solution of the linear equation or None
         if the equations are linearly dependent.
     """
-    nn = aa.shape[0]
-    xx = np.zeros((nn,), dtype=float)
 
-   
-    
     """Solving Matrix"""
-    
-    for j in range(0,nn):
-        for i in range(j+1,nn):
-            if aa[i,j] == 0:
+    nn = aa.shape[0]
+    for j in range(0, nn):
+        for i in range(j+1, nn):
+            if aa[i, j] == 0:
                 continue
-            bb[i] = aa[j, j] / aa[i,j] * bb[i] - bb[j]            
-            aa[i] = aa[j, j] / aa[i,j] * aa[i] - aa[j]
-        
-        
+            bb[i] = aa[j, j] / aa[i, j] * bb[i] - bb[j]
+            aa[i] = aa[j, j] / aa[i, j] * aa[i] - aa[j]
+
     """Solution-Vector"""
-    for k in reversed(range(0,nn)):
-        for g in reversed(range(k+1,nn)):
-            bb[k] = bb[k] - aa[k,g] * xx[g]
-        if aa[k ,k] == 0:
+    xx = np.zeros((nn,), dtype=float)
+    for k in reversed(range(0, nn)):
+        for g in reversed(range(k+1, nn)):
+            bb[k] = bb[k] - aa[k, g] * xx[g]
+        if aa[k, k] == 0:
             continue
-        xx[k] = bb[k] / aa[k ,k]
-            
-    return aa, bb, xx
-    
-    
-    
-aa = np.array([[2.0, 4.0, 4.0], [1.0, 2.0, -1.0], [5.0, 4.0, 2.0]])
-bb = np.array([1.0, 2.0, 4.0])
-
-aa, bb, xx = gaussian_eliminate(aa, bb)
-
-print(aa, bb, xx)
-    
+        xx[k] = bb[k] / aa[k, k]
+    return xx
--- test_solvers.py
+++ test_solvers.py
@@ -2,44 +2,38 @@
 """Contains routines to test the solvers module"""
 
 import numpy as np
+import pytest
 import solvers
 
-def main():
-    """Main testing function."""
+
+def test_gaussian_eliminate_1():
+    """Test gaussian_eliminate-Funktion"""
 
     print("\nTest 1")
     aa = np.array([[2.0, 4.0, 4.0], [5.0, 4.0, 2.0], [1.0, 2.0, -1.0]])
     bb = np.array([1.0, 4.0, 2.0])
     xx_expected = np.array([0.666666666666667, 0.416666666666667, -0.5])
     xx_gauss = solvers.gaussian_eliminate(aa, bb)
-    _check_result(xx_expected, xx_gauss)
+    assert np.all(np.abs(xx_gauss - xx_expected) < 1e-10)
 
+
+def test_gaussian_eliminate_2():
     print("\nTest 2")
     aa = np.array([[2.0, 4.0, 4.0], [1.0, 2.0, -1.0], [5.0, 4.0, 2.0]])
     bb = np.array([1.0, 2.0, 4.0])
     xx_expected = np.array([0.666666666666667, 0.416666666666667, -0.5])
     xx_gauss = solvers.gaussian_eliminate(aa, bb)
-    _check_result(xx_expected, xx_gauss)
+    assert np.all(np.abs(xx_gauss - xx_expected) < 1e-10)
 
+
+def test_gaussian_eliminate_3():
     print("\nTest 3")
     aa = np.array([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]])
     bb = np.array([1.0, 2.0, 3.0])
     xx_expected = None
     xx_gauss = solvers.gaussian_eliminate(aa, bb)
-    _check_result(xx_expected, xx_gauss)
-
-
-def _check_result(expected, obtained):
-    """Checks results by printing expected and obtained one."""
-    print("Expected:", expected)
-    print("Obtained:", obtained)
+    assert np.all(np.abs(xx_gauss - xx_expected) < 1e-10)
 
 
 if __name__ == '__main__':
-    main()
-    
-    
-    
-#change for testing git
-    
-#change for secont test
+    pytest.main()
